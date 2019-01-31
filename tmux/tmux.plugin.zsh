c28f7edea1b0dba5f2e79a7098e014b03003cbb3
Yigit Sever <yigit.sever@tedu.edu.tr>
2019-01-31 15:25:05 +0300

    Test not autostarting tmux

--- tmux/tmux.plugin.zsh
+++ tmux/tmux.plugin.zsh
@@ -14,7 +14,7 @@ alias tkss='tmux kill-session -t'
 
 # CONFIGURATION VARIABLES
 # Automatically start tmux
-: ${ZSH_TMUX_AUTOSTART:=true}
+: ${ZSH_TMUX_AUTOSTART:=false}
 # Only autostart once. If set to false, tmux will attempt to
 # autostart every time your zsh configs are reloaded.
 : ${ZSH_TMUX_AUTOSTART_ONCE:=true}
